f92a10cc94c3c5779f14f02695efdf63ca2e21bc
IT Academy <premiumtechnologyacademy@gmail.com>
2020-09-22 20:21:02 +0300

    TypeScript compiling

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,14 +5,14 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const convertPoundsToKg = (lbs) => {
+const convertPoundsToKg = (lbs: number) => {
     const ONE_POUND_IN_KG = 0.45359237;
     return lbs * ONE_POUND_IN_KG;
 }
 
-rl.question('Pounds: ', (lbs) => {
+rl.question('Pounds: ', (lbsStr: string) => {
+    const lbs = parseInt(lbsStr);
     const kgs = convertPoundsToKg(lbs);
     console.log("Kilograms: ", kgs)
     rl.close();
 });
-
